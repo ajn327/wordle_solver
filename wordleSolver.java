8e463cc48eb236c6b24228cfe5e53b028bebd2f3
Aaron <ajn327@lehigh.edu>
2025-09-25 22:06:31 -0400

    adding buffer reader

---
+++ wordleSolver.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class wordleSolver {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+		String st;
+    }
+}
