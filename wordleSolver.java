29b9131ff949536c762f8ea437cbe0e9d9895f23
Aaron <ajn327@lehigh.edu>
2025-09-25 22:07:28 -0400

    added file

--- wordleSolver.java
+++ wordleSolver.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class wordleSolver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        File file = new File("/Users/aaronng/Desktop/Java Workspace/Test/src/5 Letter Words");
         BufferedReader br = new BufferedReader(new FileReader(file));
 		String st;
     }
